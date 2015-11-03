6835bbf38f36672be0aeb83c67e8f387b924c7b7
ZielonyBuszmen <jozefino2009@o2.pl>
2015-11-03 15:59:38 +0100

    Dodanie bootstrapa, stworzenie folderów i dodanie kliku plików.

---
+++ funkcje/head.php
@@ -0,0 +1,9 @@
+<?php
+// plik zawiera sekcję <head> z <html>
+?>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Projekt Sklep</title>
+        <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+        <link href="bootstrap/css/flat-ui.css" rel="stylesheet">
+    </head>
--- index.php
+++ index.php
@@ -1,12 +1,49 @@
+<?php session_start(); 
+    //rozpoczęcie sesji 
+    
+    // dołączenie plików z funkcjami
+    // dołącznie pliku z funkcjami do dołącznia plilów
+    require_once("funkcje/f_include.php");
+    
+?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Projekt Sklep</title>
-    </head>
-    <body>
-        <?php
-        // srhsifdbidifsf
-        ?>
+        
+<?php 
+    // dołączenie <head> witryny
+    include("funkcje/head.php");
+?>  
+    
+    <body>     
+        
+        
+        
+        
+        
+        
+        
+        Trzeba zrobić główny layout, jak ma to wyglądać wszystko
+        <!-- Tutaj zaczyna się treść danej podstrony -->
+        <!-- Całość jest zawarta w containerze oraz w jednym row -->
+        <div class="container">
+            <div id="z_panel_calosc" class="row">
+                 Jakas treść
+            </div> 
+        </div>    
+
+        
+        
+        
+        
+        
+        
+        
+        
+        <!-- Dołączenie skryptów z bootstrapa i jQuery -->
+        <script src="bootstrap/js/jquery.min.js"></script>
+        <script src="bootstrap/js/bootstrap.js"></script>
+        <script src="bootstrap/js/jquery.validate.js"></script>
+        <script src="bootstrap/js/walidacja.js"></script>
+        <script src="bootstrap/js/flat-ui.js"></script>
     </body>
 </html>
