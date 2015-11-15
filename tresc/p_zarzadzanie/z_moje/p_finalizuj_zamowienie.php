bc44bc5e8cd96aa86b12ca13a9d6f6e90b7dffa1
ZielonyBuszmen <jozefino2009@o2.pl>
2015-11-15 23:16:13 +0100

    Dodanie info do zarządzania zamówieniem.

---
+++ tresc/p_zarzadzanie/z_moje/p_finalizuj_zamowienie.php
@@ -0,0 +1,10 @@
+skrypt sprawdza, czy można sfinalizować zamówienie. Jeśli tak, robi to:<br>
+odejmuje produkty z bazy, itepe
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
